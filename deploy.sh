49ee043d959828a6b74351553ccc2a7e446a4109
dongweiwei <v_dongweiwei@bilibili.co>
2022-08-05 15:26:06 +0800

    ..

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:CyanDong/-vuepress-.git main:blog-pages
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>s
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:CyanDong/-vuepress-.git master:gh-pages
 
 cd -
