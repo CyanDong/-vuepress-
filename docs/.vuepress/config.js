c984fe0e9a6c1a1296ec7b75044d7f750f24c83c
dongweiwei <v_dongweiwei@bilibili.co>
2022-08-05 16:42:40 +0800

    ..

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,28 +6,13 @@ module.exports = {
             lang: "zh-CN",
         },
       },
-    theme: "reco",
+    theme: "vuepress-theme-reco",
     themeConfig: {
         nav: [
-            { text: '首页', link: '/' },
-            { 
-                text: 'Jessie的博客', 
-                items: [
-                    { text: 'Github', link: 'https://github.com/Jessie-jzn' },
-                    { text: 'CSDN', link: 'https://blog.csdn.net/zn740395858?spm=1010.2135.3001.5343' },
-                    { text: '掘金', link: 'https://juejin.cn/user/2524134425764375' }
-                ]
-            }
+          { text: 'Home', link: '/' },
+          { text: 'Guide', link: '/guide/' },
+          { text: 'External', link: 'https://google.com' },
         ],
-        // sidebar:[
-        //     {
-        //         title: "博客搭建",
-        //         path: "/construction/Blog1",
-        //         collapsable: false, // 不折叠
-        //         children: [
-        //             { title: "博客 01", path: "/construction/Blog1" },
-        //         ],
-        //     }
-        // ]
-    }
+        sidebar: 'auto'
+      }
   }
